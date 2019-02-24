b802884a9f958f6dc51dd25095e5db456b16fa13
MartinsCudars <martins.cudars@gmail.com>
2019-02-24 18:01:35 +0200

    Modify bio block

---
+++ src/js/components/Bio.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="block block-bio">
+    <div class="block__inner bio__inner" v-html="content">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+  name: 'Bio',
+
+  data () {
+    return {
+      content: '',
+      loaded: false
+    }
+  },
+  mounted () {
+    this.$http
+      .get('/rest/blocks/bio.json')
+      .then(response => (
+        this.content = response.data[0].bio,
+        this.loaded = true
+    ))
+  },
+}
+</script>
+
+<style lang="css" scoped>
+</style>
