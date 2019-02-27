41c5000b6d67e16eddc0f9be9349e78dc6acb635
MartinsCudars <martins.cudars@gmail.com>
2019-02-27 23:25:37 +0200

    Add footer

---
+++ src/js/components/SiteFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="block block-footer">
+    <div class="block__inner">
+      All Rights Reserved. 2019
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+  name: 'SiteFooter',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../scss/global/variables';
+  .block-footer {
+    background-color: darken($main, 12%);
+    color: $white;
+    text-align: center;
+  }
+</style>
