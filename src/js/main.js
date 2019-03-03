5e3a868602c40e36a9c45f76abb37fdd7e53b3c8
MartinsCudars <martins.cudars@gmail.com>
2019-03-03 15:39:12 +0200

    Add vue-lazyload

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,14 @@ import '../scss/main.scss';
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
- 
+import VueLazyload from 'vue-lazyload'
+
 Vue.prototype.$http = axios
 
+Vue.use(VueLazyload, {
+  preLoad: 1.25,
+  attempt: 1
+})
 
 import FrontPage from './components/FrontPage'
 import SiteFooter from './components/SiteFooter'
